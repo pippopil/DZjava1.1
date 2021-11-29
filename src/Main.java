f5846bf2320a902662f30bd8f66cc93986339c9b
Nikolay Abadeev <voood234@gmail.com>
2021-11-29 16:27:14 +0300

    Add

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      int currentAccount = 2_000_000_000;
+      int transferAmount = 500_000_000;
+      int total = currentAccount + transferAmount;
+        System.out.println(total);
+    }
+}
